7098d30d840a8c71c6ad4a8fde42890a8e49c0c2
Rahul <rahulbiswalnew@gmail.com>
2023-06-22 23:41:11 +0530

    interceptors

--- src/app/interceptors/http-error.interseptor.ts
+++ src/app/interceptors/http-error.interseptor.ts
@@ -1,24 +1,27 @@
-import { ObserversModule } from "@angular/cdk/observers";
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { catchError, observable } from "rxjs";
+import {
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+  } from '@angular/common/http';
+  import { Injectable } from '@angular/core';
+  import { Observable, throwError as observableThrowError } from 'rxjs';
+  import { catchError } from 'rxjs/operators';
+
 
 @Injectable()
 export class HttpErrorsInterceptor implements HttpInterceptor {
-    constructor(){}
+  constructor() {}
 
-    intercept(
-        req: HttpRequest<any>,
-        next: HttpHandler
-    ): observable<HttpEvent<any>> {
-        return next.handle(req).pipe(
-            catchError((err) => {
-                console.log(err);
-                return ObservableThrowError(err)
-            })
-        );
-    }
-}
-
-function ObservableThrowError(err: any): any {
-    throw new Error("Function not implemented.");
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((err) => {
+        console.log(err);
+        return observableThrowError(err);
+      })
+    );
+  }
 }
